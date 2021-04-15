91cdb9f5adee3f3c6bf413917f653468ce36f4ee
Daniel <moesgaarda@gmail.com>
2021-04-15 10:42:09 +0200

    Updates

--- model/LGCN-TensorFlow/utility/load_data.py
+++ model/LGCN-TensorFlow/utility/load_data.py
@@ -33,8 +33,8 @@ class Data(object):
         if 'yelpnc' in self.path:
             self.user_column_name = 'user_id'
             self.item_column_name = 'business_id'
-            self.context_column_list = ['date']
-            self.item_sideinfo_multihot = ['Sandwiches','Food','Bagels','Restaurants','Burgers','ChickenWings','Bars','Nightlife','SportsBars','American(Traditional)','Steakhouses','Tapas/SmallPlates','Breakfast&Brunch','American(New)','Pubs','CocktailBars','TapasBars','Gastropubs','Coffee&Tea','BeerBar','Breweries','Bakeries','Southern','SoulFood','Delis','SpecialtyFood','WineBars','EventPlanning&Services','Caterers','Cafes','Arts&Entertainment','MusicVenues','Pizza','Italian','IceCream&FrozenYogurt','FastFood','Chinese','SushiBars','Thai','AsianFusion','Japanese','Venues&EventSpaces','Mexican','Vietnamese','Hotels&Travel','Diners','ComfortFood','Salad','LocalFlavor','Seafood','NailSalons','Beauty&Spas','French','Shopping','Beer','Wine&Spirits','Barbeque','Soup','Tex-Mex','Fashion','DepartmentStores','Lounges','Vegetarian','ActiveLife','Desserts','LatinAmerican','Cinema','Vegan','Gluten-Free','Greek','Home&Garden','Drugstores','Mediterranean','JuiceBars&Smoothies','Grocery','Noodles','FoodTrucks','Indian','EthnicFood','Flowers&Gifts']
+            self.context_column_list = ['month','day_of_week','timeofday'] # 'hour'
+            self.item_sideinfo_multihot = ['Burgers','ChickenWings','Bars','Restaurants','Nightlife','SportsBars','American(Traditional)','Steakhouses','Tapas/SmallPlates','Breakfast&Brunch','American(New)','Pubs','CocktailBars','TapasBars','Gastropubs','Food','Coffee&Tea','BeerBar','Breweries','Bakeries','Southern','SoulFood','Arts&Entertainment','Sandwiches','MusicVenues','Pizza','Italian','WineBars','IceCream&FrozenYogurt','FastFood','Chinese','SushiBars','Thai','AsianFusion','Japanese','EventPlanning&Services','Venues&EventSpaces','Mexican','Vietnamese','Diners','ComfortFood','Cafes','Salad','French','Caterers','Shopping','Beer','Wine&Spirits','Delis','Barbeque','Seafood','Soup','Tex-Mex','Fashion','DepartmentStores','Lounges','Vegetarian','ActiveLife','Desserts','LatinAmerican','Cinema','Vegan','Gluten-Free','Home&Garden','Cajun/Creole','Bagels','Beauty&Spas','SpecialtyFood','Mediterranean','Grocery','Noodles','FoodTrucks','LocalFlavor','Greek','Indian','EthnicFood','BeerGardens','Korean','Flowers&Gifts']
             self.item_sideinfo_onehot = ['city']
             self.user_sideinfo_onehot = ['yelping_since', 'fans', 'average_stars']
 
@@ -53,6 +53,14 @@ class Data(object):
             self.item_sideinfo_multihot = ['cost']
             self.item_sideinfo_onehot = []
             self.user_sideinfo_onehot = []
+
+        if 'ml1m' in self.path:
+            self.user_column_name = 'userId'
+            self.item_column_name = 'movieId'
+            self.context_column_list = ['weekday','timeofday']
+            self.item_sideinfo_multihot = ['Action','Adventure','Animation','Children\'s','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War','Western']
+            self.item_sideinfo_onehot = []
+            self.user_sideinfo_onehot = ['age','gender','occupation']
 
         if self.loo_eval:
             self.init_loo_split()
@@ -330,11 +338,15 @@ class Data(object):
                     item_sideinfo_offset = self.n_users + self.n_items + self.n_user_sideinfo + onehot
                     adj_mat[itemId, item_sideinfo_offset] = 1 # US (OneHot)
                     # TODO Prøv at indsætte identity matrix i stedet
-                    adj_mat[item_sideinfo_offset, itemId] = 1 # US (OneHot)
+                    #adj_mat[item_sideinfo_offset, itemId] = 1 # USt (OneHot)
                 for multihot in item_sideinfos[1]: # Multihot
                     item_sideinfo_offset = self.n_users + self.n_items + self.n_user_sideinfo + multihot
                     adj_mat[itemId, item_sideinfo_offset] = 1 # US (Multihot)
-                    adj_mat[item_sideinfo_offset, itemId] = 1 # US (Multihot)
+                    #adj_mat[item_sideinfo_offset, itemId] = 1 # USt (Multihot)
+
+                for i in range(self.n_user_sideinfo + self.n_item_sideinfo):
+                    offset = self.n_users + self.n_items
+                    adj_mat[i + offset, i + offset] = 1
 
                 adj_mat[userId, item_offset] = 1 # R
                 adj_mat[item_offset, userId] = 1 # Rt
