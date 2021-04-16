5c996648692d83fd34f489e11cef2a7513cd3886
Rasmus <rasmusedu@gmail.com>
2021-04-16 13:26:25 +0200

    toppop context loo eval

--- model/TopPop/toppop.py
+++ model/TopPop/toppop.py
@@ -44,18 +44,49 @@ class TopPop():
 
 class TopPopContext():
     def __init__(self, dataframe_path, loo_path, userid_column_name, itemid_column_name, ks):
+        if dataframe_path == 'model/TopPop/frappe':
+            self.context_list = ['weekday', 'timeofday', 'isweekend', 'weather']
+        elif dataframe_path == 'yelpon':
+            self.context_list = ['weekday', 'timeofday', 'isweekend', 'weather']
+        elif dataframe_path =='yelpnc':
+            self.context_list = ['weekday', 'timeofday', 'isweekend', 'weather']
         self.dataframe = pd.read_csv(dataframe_path + '.txt', sep=',', )
         self.loo_df = pd.read_csv(loo_path + '.txt', sep=',', )
-        self.evaluator = evaluator()
 
+       # self.evaluator = evaluator()
+       # Get popular items in case the context does not have enough items to fill a list of k size
         pop_series = self.dataframe[itemid_column_name].value_counts()
+        pop_ids = pop_series.index.values.tolist()
+        user_top_k_dict = {}
+
+        for _, row in self.loo_df.iterrows():          
+            context_df = self.loo_df
+            # Get the current context of the interaction
+            interaction_context = []
+            for context in self.context_list:
+                interaction_context.append(row[context]) 
+            
+            # Filter test set such that only interactions in that context appear
+            for index in range(len(self.context_list)):
+                context_df = context_df[(context_df[self.context_list[index]] == interaction_context[index])]
+
+            # Make initial top k dict from the popular items in the context
+            pop_context_series = context_df[itemid_column_name].value_counts()
+            user_top_k_dict[row[userid_column_name]] = pop_context_series.index.values.tolist()
+        
+        
+        user_ground_truth_dict = construct_user_ground_truth_dict(self.loo_df, userid_column_name, itemid_column_name)
         for k in ks:
-            k_pop_series = pop_series.head(k)
-            k_pop_ids = k_pop_series.index.values.tolist()
-            user_ground_truth_dict = construct_user_ground_truth_dict(self.test_df, userid_column_name, itemid_column_name)
-            user_top_k_dict = {}
-            for id in self.test_df[userid_column_name].unique():
-                user_top_k_dict[id] = k_pop_ids
+            # Ensure top k dick has values that are k length
+            # If the value is shorter than k, pad it with the most popular items not already in the list, regardless of context
+            for key, value in user_top_k_dict.items():
+                if len(value) < k:
+                    for id in pop_ids:
+                        if id not in value and len(user_top_k_dict[key]) < k:
+                            user_top_k_dict[key].append(id)    
+                else:
+                    k_list = value[:k]
+                    user_top_k_dict[key] = user_top_k_dict[key][:k]
 
             hr, ndcg, mrr = evaluator.evaluate_loo_no_sort(self.evaluator, user_top_k_dict, user_ground_truth_dict, k)
 
@@ -64,9 +95,10 @@ class TopPopContext():
             print(f"NDCG: {ndcg}")
             print(f"MRR: {mrr}")
 
-def construct_user_ground_truth_dict(loo_df, userid_column_name, itemid_column_name):
+            
+def construct_user_ground_truth_dict(dataframe, userid_column_name, itemid_column_name):
     user_ground_truth_dict = {}
-    for _, row in loo_df.iterrows():
+    for _, row in dataframe.iterrows():
             # Add interactions as ground truths
             if row[userid_column_name] not in user_ground_truth_dict:
                 user_ground_truth_dict[row[userid_column_name]] = [
@@ -84,3 +116,4 @@ def construct_user_ground_truth_dict(loo_df, userid_column_name, itemid_column_n
 #TopPop('yelpnc', 'yelpnctest', 'user_id', 'business_id', [20, 50, 100])
 #TopPop('yelpon', 'yelpontest', 'user_id', 'business_id', [20, 50, 100])
 #TopPop('frappe', 'frappetest', 'userId', 'movieId', [20, 50, 100])
+TopPopContext('model/TopPop/frappe', 'model/TopPop/frappetest', 'user', 'item', [20, 50, 100])
